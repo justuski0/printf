1d235f666143585f89876f7ee5aabc6e3a67f7ad
Mutisomary <125799629+Mutisomary@users.noreply.github.com>
2023-04-23 20:56:39 +0300

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/************************* WRITE HANDLE *************************/
 /**
+ * By Justus and Mary
  * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
