eac4837235dcb995ef528e80fc807b8cc77a8ee0
Mutisomary <125799629+Mutisomary@users.noreply.github.com>
2023-04-23 20:50:27 +0300

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * By Justus and Mary
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
